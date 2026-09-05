f6853768e9ff867b15bcf4c9afd633ca43653cb2
PalanisamyPM <ppalanisamy17022@gmail.com>
2026-09-05 18:16:08 +0530

    Change API base URL to production endpoint

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:5000/api",
+    baseURL: "https://code-craft-ai-jet.vercel.app/",
     headers: {
         "Content-Type": "application/json"
     }
