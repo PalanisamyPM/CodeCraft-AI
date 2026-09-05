3399d3fad70b2c91610b8224f5a43a1f4940de38
PalanisamyPM <ppalanisamy17022@gmail.com>
2026-09-05 18:19:28 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://code-craft-ai-jet.vercel.app/",
+    baseURL: "http://127.0.0.1:5000/api",
     headers: {
         "Content-Type": "application/json"
     }
